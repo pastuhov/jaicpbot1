6e4f441ab5fea253c597e9fec7a749080020952f
kirill@pastukhov.su <kirill@pastukhov.su>
2023-04-26 11:43:38 +0000

    Edited files:     /src/pipedream.js

--- src/pipedream.js
+++ src/pipedream.js
@@ -1,9 +1,10 @@
 var ENDPOINT = $injector.pipedream_endpoint;
 
 function sendRequest(payload){
-return $http.get("${ENDPOINT}", {
+return $http.get("${endpoint}?payload=${payload}", {
         timeout: 10000,
         query:{
+            endpoint: ENDPOINT,
             payload: payload
         }
     });
